0a8214168d08491fd2b5f242e6b6cecabd426f25
Erhan BICER <iletisim@erhanbicer.com>
2022-03-25 16:31:04 +0300

    chore: optimize import

--- react/features/filmstrip/components/native/Thumbnail.js
+++ react/features/filmstrip/components/native/Thumbnail.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import {Dimensions, View} from 'react-native';
+import {View} from 'react-native';
 import type { Dispatch } from 'redux';
 
 import { ColorSchemeRegistry } from '../../../base/color-scheme';
